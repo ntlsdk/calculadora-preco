8c9a5f7475e35d9e0e876a45b7a8248c3b2d79da
ntlsdk <124364927+ntlsdk@users.noreply.github.com>
2023-03-12 19:47:19 -0300

    Add files via upload

---
+++ CalculadoraProdutos.py
@@ -0,0 +1,4 @@
+preco1 = float(input("Digite o preço do primeiro produto:"))
+preco2 = float(input("Digite o preço do segundo produto:"))
+precoTotal = preco1 + preco2
+print("Preço total:", precoTotal)
